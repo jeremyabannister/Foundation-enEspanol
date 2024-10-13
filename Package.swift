e76610c0481791f846c4bc65c9230ac9189073cd
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2024-10-13 14:38:50 +0200

    build: update to Swift-enEspanol version 0.1.1

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/jeremyabannister/Swift-enEspanol",
-            "0.1.0" ..< "0.2.0"
+            .upToNextMinor(from: "0.1.1")
         )
     ],
     targets: [
